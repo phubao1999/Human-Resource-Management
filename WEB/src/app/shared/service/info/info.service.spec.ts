09025b541b034657fd0d0748c5b87865e718e79e
Bao_PT <phutuongbao1999@gmail.com>
2020-03-29 00:55:23 +0700

    Fix Service

--- WEB/src/app/shared/service/info/info.service.spec.ts
+++ WEB/src/app/shared/service/info/info.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import { InfoService } from './info.service';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('InfoService', () => {
+  let service: InfoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    service = TestBed.inject(InfoService);
   });
 
   it('should be created', () => {
